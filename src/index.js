6429c9857154d34400075e0c810fd744196e0492
Jeff Long <jlong@Jeffreys-MacBook-Pro.local>
2021-12-16 22:30:09 -0500

    4_5:added x/y axis w/ ticks

--- src/index.js
+++ src/index.js
@@ -5,12 +5,31 @@ const scatterData = [{friends: 5, salary: 22000},
   {friends: 0, salary: 180000}, {friends: 27, salary: 56000},
   {friends: 8, salary: 74000}];
 
+
+const xExtent = d3.extent(scatterData, function(d) {
+  return d.salary;
+});
+const yExtent = d3.extent(scatterData, function(d) {
+  return d.friends;
+});
+const xScale = d3.scaleLinear().domain(xExtent).range([0, 500]);
+const yScale = d3.scaleLinear().domain(yExtent).range([0, 500]);
 d3.select('svg').selectAll('circle')
     .data(scatterData).enter()
     .append('circle').attr('r', 5)
-    .attr('cx', function(d, i) {
-      return i * 10;
+    .attr('cx', function(d) {
+      return xScale(d.salary);
     })
     .attr('cy', function(d) {
-      return d.friends;
+      return yScale(d.friends);
     });
+
+const yAxis = d3.axisRight(yScale);
+d3.select('svg').append('g').attr('id', 'yAxisG').call(yAxis);
+const xAxis = d3.axisBottom(xScale);
+d3.select('svg').append('g').attr('id', 'xAxisG').call(xAxis);
+
+// var yAxis = d3.svg.axis().scale(yScale).orient("right");
+// d3.select("svg").append("g").attr("id", "yAxisG").call(yAxis);
+// var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+// d3.select("svg").append("g").attr("id", "xAxisG").call(xAxis);
